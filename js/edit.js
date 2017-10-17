bdd99c48987c64924d766abbaa34574cdecdd8f5
Patrick Baker <pbaker@stilo.com>
2017-10-17 18:58:35 -0400

    edit.js and build

--- js/edit.js
+++ js/edit.js
@@ -1,15 +1,19 @@
 function frame_loaded() {
     if (location.pathname.indexOf('/dev/') !== -1)
     {
-        var h1   = window.frames["contentwin"].document.getElementsByClassName("topictitle1");
-        if (!h1)
+        var h1   = window.frames["contentwin"].document.getElementsByTagName("H1");
+
+        if (!h1 || h1.length == 0)
             return;
+
+        var meta = window.frames["contentwin"].document.querySelector('meta[name="filepath"]');
+        var path = meta && meta.getAttribute("content");
 
         var a    = document.createElement("a");
         var span = document.createElement("span"); 
 
         a.appendChild(document.createTextNode("(Edit this page)")); 
-        a.href ="http://authorbridge?scope=GitHub&path=pgbsandbox:pgbsandbox.github.io:master:/DITA/add-figure.dita"
+        a.href ="http://authorbridge?scope=GitHub&path=pgbsandbox:pgbsandbox.docs:master:/DITA/" + path
         span.innerHTML += "&nbsp;&nbsp;&nbsp;"
         span.style.fontSize = "1.1rem";
         span.appendChild(a);
